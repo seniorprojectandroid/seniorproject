1086403f6636cb977f052a80aa6943ed9c9666aa
joeldys ortiz <jorti063@fiu.edu>
2012-09-08 15:01:50 -0400

    FacebookLoginActivity

---
+++ seniorproject/src/edu/fiu/cs/seniorproject/FacebookLoginActivity.java
@@ -0,0 +1,42 @@
+package edu.fiu.cs.seniorproject;
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import com.facebook.android.*;
+import com.facebook.android.Facebook.*;
+
+public class FacebookLoginActivity extends Activity {
+
+    Facebook facebook = new Facebook("378790605525916");
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+
+        facebook.authorize(this, new DialogListener() {
+            @Override
+            public void onComplete(Bundle values) {}
+
+            @Override
+            public void onFacebookError(FacebookError error) {}
+
+            @Override
+            public void onError(DialogError e) {}
+
+            @Override
+            public void onCancel() {}
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        facebook.authorizeCallback(requestCode, resultCode, data);
+    }
+}
+
--- seniorproject/src/edu/fiu/cs/seniorproject/MainActivity.java
+++ seniorproject/src/edu/fiu/cs/seniorproject/MainActivity.java
@@ -1,10 +1,18 @@
 package edu.fiu.cs.seniorproject;
 
+
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	
+	
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,4 +25,6 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+
 }
