4274bede198d3baa05df7b28886e35c30b9ec1d1
Julio <jreye087@fiu.edu>
2012-09-15 15:28:10 -0400

    Implement RestClient methods.

--- seniorproject/src/edu/fiu/cs/seniorproject/client/RestClient.java
+++ seniorproject/src/edu/fiu/cs/seniorproject/client/RestClient.java
@@ -1,30 +1,162 @@
+/*
+ * Copyright 2010 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package edu.fiu.cs.seniorproject.client;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import edu.fiu.cs.seniorproject.utils.Logger;
 
 import android.os.Bundle;
 
 public class RestClient {
 	
-	private String appId = null;
+	private static final String CHAR_SET = "UTF-8";
 	
+	protected String appId = null;
 	
 	public RestClient(String appID)
 	{
 		this.appId = appId;
 	}
 	
-	public static String encodeUrl(Bundle parameters)
-	{
-		
-		return "";
+	protected String openUrl(String url, String method, Bundle params)
+				throws MalformedURLException, IOException {
+	
+		// random string as boundary for multi-part http post
+        String strBoundary = "3i2ndDfv2rTHiSisAbouNdArYfORhtTPEefj3q2f";
+        String endLine = "\r\n";
+
+        OutputStream os;
+
+        if (method.equals("GET")) {
+            url = url + "?" + encodeUrl(params);
+        }
+        Logger.Info("Rest Client Open Url ", method + " URL: " + url);
+        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+        conn.setRequestProperty("User-Agent", System.getProperties().getProperty("http.agent") + " FacebookAndroidSDK");
+        
+        if (!method.equals("GET")) {
+            Bundle dataparams = new Bundle();
+            for (String key : params.keySet()) {
+                Object parameter = params.get(key);
+                if (parameter instanceof byte[]) {
+                    dataparams.putByteArray(key, (byte[])parameter);
+                }
+            }
+            
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty( "Content-Type", "multipart/form-data;boundary="+strBoundary);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.connect();
+            os = new BufferedOutputStream(conn.getOutputStream());
+
+            os.write(("--" + strBoundary +endLine).getBytes());
+            os.write((encodePostBody(params, strBoundary)).getBytes());
+            os.write((endLine + "--" + strBoundary + endLine).getBytes());
+
+            if (!dataparams.isEmpty()) {
+
+                for (String key: dataparams.keySet()){
+                    os.write(("Content-Disposition: form-data; filename=\"" + key + "\"" + endLine).getBytes());
+                    os.write(("Content-Type: content/unknown" + endLine + endLine).getBytes());
+                    os.write(dataparams.getByteArray(key));
+                    os.write((endLine + "--" + strBoundary + endLine).getBytes());
+
+                }
+            }
+            os.flush();
+        }
+
+        String response = "";
+        try {
+            response = read(conn.getInputStream());
+        } catch (FileNotFoundException e) {
+            // Error Stream contains JSON that we can parse to a FB error
+            response = read(conn.getErrorStream());
+        }
+        return response;
 	}
 	
-	  
-	public static String openUrl(String url, String method, Bundle params)
-									throws MalformedURLException, IOException {
-		  
-		  return "";
+	private String encodeUrl(Bundle parameters)
+	{
+		if (parameters == null) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (String key : parameters.keySet()) {
+            Object parameter = parameters.get(key);
+            if (!(parameter instanceof String)) {
+                continue;
+            }
+
+            if (first) first = false; else sb.append("&");
+            String encodedKey;
+            String encodedValue;
+			try {
+				encodedKey = URLEncoder.encode(key, CHAR_SET);
+				encodedValue = URLEncoder.encode(parameters.getString(key),CHAR_SET);
+	            sb.append( encodedKey + "=" + encodedValue );
+			} catch (UnsupportedEncodingException e) {
+				Logger.Warning("Exception encoding params!!!");
+			}
+        }
+        return sb.toString();
 	}
+	
+	private String encodePostBody(Bundle parameters, String boundary) {
+        if (parameters == null) return "";
+        StringBuilder sb = new StringBuilder();
+
+        for (String key : parameters.keySet()) {
+            Object parameter = parameters.get(key);
+            if (!(parameter instanceof String)) {
+                continue;
+            }
+
+            sb.append("Content-Disposition: form-data; name=\"" + key +
+                    "\"\r\n\r\n" + (String)parameter);
+            sb.append("\r\n" + "--" + boundary + "\r\n");
+        }
+
+        return sb.toString();
+    }
+	
+	private String read(InputStream in) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader r = new BufferedReader(new InputStreamReader(in), 1000);
+        for (String line = r.readLine(); line != null; line = r.readLine()) {
+            sb.append(line);
+        }
+        in.close();
+        return sb.toString();
+    }
 }
