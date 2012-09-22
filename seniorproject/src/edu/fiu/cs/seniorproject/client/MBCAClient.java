f98cb411866623b6c7c948820f4539246ad7cb85
c4t <llanz001@fiu.edu>
2012-09-22 18:12:33 -0400

    mbca provider and client alpha

---
+++ seniorproject/src/edu/fiu/cs/seniorproject/client/MBCAClient.java
@@ -0,0 +1,122 @@
+package edu.fiu.cs.seniorproject.client;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import edu.fiu.cs.seniorproject.utils.Logger;
+
+import android.os.Bundle;
+
+public class MBCAClient extends RestClient{
+
+	public MBCAClient(String appId)
+	{
+		super(appId);
+	}
+	
+	/**
+	 * 
+	 * @param reference
+	 * @param eventId
+	 * @return String
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
+	public String getEvent(String reference, String eventId) throws MalformedURLException, IOException
+	{
+		/*
+		 * 	http://www.miamibeachapi.com/api/index.php/search/solodev_view?qry={start_time:{$gt:CURRENTTIME},datatable_category_id:{$exists:true},%20calendar_id:1}
+		 * 
+		 * 	https://maps.googleapis.com/maps/api/place/event/details/format?sensor=true_or_false
+  		 *	&key=api_key
+  		 *	&reference=CnRkAAAAGnBVNFDeQoOQHzgdOpOqJNV7K9-etc
+  		 *	&event_id=weafgerg1234235
+		 */
+		Bundle params = this.getBundle();
+		
+		if(reference != null)
+			params.putString("reference", reference);
+		if(eventId != null)
+			params.putString("event_id", eventId);
+		
+		String result = null;
+		String url = "http://www.miamibeachapi.com/api/index.php/search/solodev_view?qry={start_time:{$gt:CURRENTTIME},datatable_category_id:{$exists:true},%20calendar_id:1}"; //"https://maps.googleapis.com/maps/api/place/event/details/json";
+		String method = "GET";
+
+		try
+		{
+			result = this.openUrl(url, method, params);
+		
+		}catch(MalformedURLException e)
+		{
+			Logger.Error("MBCAClient getEvents MalformedURLException");
+		}catch(IOException e)
+		{
+			Logger.Error("MBCAClient getEvents IOException");
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * 
+	 * @param longitude
+	 * @param latitude
+	 * @param radius
+	 * @param types
+	 * @param name
+	 * @return String
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
+	public String getPlaces(String longitude, String latitude, String radius, String types, String name) throws MalformedURLException, IOException
+	{
+	   /*
+	    * https://maps.googleapis.com/maps/api/place/search/json?
+		* location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&
+		* sensor=false&key=AddYourOwnKeyHere
+		*/
+
+
+		Bundle params = this.getBundle();
+		
+		if(longitude != null)
+			params.putString("longitude", longitude);
+		if(latitude != null)
+			params.putString("latitude", latitude);
+		if(radius != null)
+			params.putString("radius",radius);
+		if(types != null)
+			params.putString("types",types);
+		if(name != null)
+			params.putString("name",name);
+		
+		params.putString("sensor","false");
+		
+		String result = null;
+		String url = "http://www.miamibeachapi.com/api/index.php/search/solodev_view?qry={start_time:{$gt:CURRENTTIME},datatable_category_id:{$exists:true},%20calendar_id:1}";
+		String method = "GET";
+		
+		try
+		{
+			result = this.openUrl(url, method, params);
+		
+		}catch(MalformedURLException e)
+		{
+			Logger.Error("MBCAClient getPlaces MalformedURLException");
+		}catch(IOException e)
+		{
+			Logger.Error("MBCAClient getPlaces IOException");
+		}
+		
+		return result;
+	}
+	
+	public Bundle getBundle()
+	{
+		Bundle bundle = new Bundle();
+		bundle.putString("key", this.appId);
+		return bundle;
+	}
+	
+}
---
+++ seniorproject/src/edu/fiu/cs/seniorproject/data/provider/MBCAProvider.java
@@ -0,0 +1,114 @@
+package edu.fiu.cs.seniorproject.data.provider;
+
+import java.io.IOException;
+//import java.lang.reflect.Array;
+import java.net.MalformedURLException;
+//import java.util.Iterator;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+//import android.provider.ContactsContract.CommonDataKinds.Event;
+
+import edu.fiu.cs.seniorproject.client.MBCAClient;
+import edu.fiu.cs.seniorproject.config.AppConfig;
+import edu.fiu.cs.seniorproject.utils.Logger;
+
+public abstract class MBCAProvider implements DataProvider 
+{
+	private MBCAClient MBCAClient;
+	
+	public MBCAProvider()
+	{
+		this.MBCAClient  = new MBCAClient(AppConfig.MBCA_APP_ID);
+	}
+
+	public List<?> parseEvent(String reference, String eventId) throws MalformedURLException, IOException, JSONException  {
+		
+		String result = null;
+		JSONObject data = null;
+
+		try 
+		{
+			result = this.MBCAClient.getEvent(reference, eventId);
+		} 
+		catch (MalformedURLException e) 
+		{
+			Logger.Error("MalformedURLException in parseEvent");
+		} 
+		catch (IOException e) 
+		{
+			Logger.Error("IOException in parseEvent");
+		}
+		
+		try
+		{
+			data = new JSONObject(result);
+		}
+		catch(JSONException e)
+		{
+			Logger.Error("JSONException in parseEvent");
+		}
+		
+		JSONArray eventDetails = data.getJSONArray("result");
+		
+		return (List<?>)eventDetails;
+		
+	}
+	
+	public List<?> parsePlaces(String longitude, String latitude, String radius, String types, String name) throws MalformedURLException, IOException
+	{
+		
+		String result = null;
+		JSONObject data = null;
+		
+		try 
+		{
+			result = this.MBCAClient.getPlaces(longitude, latitude, radius, types, name);
+		} 
+		catch (MalformedURLException e) 
+		{
+			Logger.Error("MalformedURLException in parseEvent");
+		} 
+		catch (IOException e) 
+		{
+			Logger.Error("IOException in parseEvent");
+		}
+		
+		try
+		{
+			data = new JSONObject(result);
+		}
+		catch(JSONException e)
+		{
+			Logger.Error("JSONException in parseEvent");
+		}
+		
+//		try{
+				    //Get the element that holds the earthquakes ( JSONArray )
+//				    JSONArray  earthquakes = data.getJSONArray("earthquakes");
+				 
+				            //Loop the Array
+//				        for(int i=0;i < earthquakes.length();i++){                      
+//				 
+//				            HashMap<String, String> map = new HashMap<String, String>();
+//				            JSONObject e = earthquakes.getJSONObject(i);
+//				 
+//				            map.put("id",  String.valueOf(i));
+//				            map.put("name", "Earthquake name:" + e.getString("eqid"));
+//				            map.put("magnitude", "Magnitude: " +  e.getString("magnitude"));
+//				            mylist.add(map);
+//				    }
+//				    }catch(JSONException e)        {
+//				         Log.e("log_tag", "Error parsing data "+e.toString());
+//				    }
+		
+		
+		return null;
+//		}
+	}
+	
+	
+}
