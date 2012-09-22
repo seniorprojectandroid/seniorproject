d34026e7c95bb89589c33d92125c3f5aab0186eb
roberto <risaa002@fiu.edu>
2012-09-22 13:35:23 -0400

    intermit commit.

--- seniorproject/src/edu/fiu/cs/seniorproject/data/provider/DataProvider.java
+++ seniorproject/src/edu/fiu/cs/seniorproject/data/provider/DataProvider.java
@@ -1,11 +1,24 @@
 package edu.fiu.cs.seniorproject.data.provider;
 
+import java.util.LinkedList;
+
+import edu.fiu.cs.seniorproject.data.Event;
+import edu.fiu.cs.seniorproject.data.Place;
+
 
 
 public interface DataProvider {
 	
 	 public String getEvent();
-	
+	 
 	 public String getPlaces();
+	 
+	 public LinkedList<Event> getEventList(String query);
+	 
+	 public LinkedList<Place> getPlaceList(String query);
+	 
+	 public Event getEventDetails(String location, String eventId);
+	 
+	 public Place getPlaceDeetails(String location, String placeId);
 
 }
