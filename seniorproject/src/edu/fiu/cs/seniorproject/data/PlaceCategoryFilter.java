585cb0606ab377c688cb1c36168cfd5d3b67d616
Julio <jreye087@fiu.edu>
2012-10-06 23:29:40 -0400

    Add event list activity filters

---
+++ seniorproject/src/edu/fiu/cs/seniorproject/data/PlaceCategoryFilter.java
@@ -0,0 +1,22 @@
+package edu.fiu.cs.seniorproject.data;
+
+public class PlaceCategoryFilter {
+
+	public static final int	BAKERY 						=	447;
+	public static final int	DENTISTS 					=	367;
+	public static final int	FOOD_SALES 					=	383;
+	public static final int	GALLERY_ART 				=	466;
+	public static final int	HOTEL 						=	360;
+	public static final int	LIQUOR_SALES 				=	470;
+	public static final int	LOCKSMITH 					=	504;
+	public static final int	PARKING_GARAGE 				=	472;
+	public static final int	PARKING_LOT 				=	401;
+	public static final int	PARKING_LOT_PROVISIONAL     =	445;
+	public static final int	PARKING_LOT_SELF_PARKING    =	394;
+	public static final int	PARKING_LOT_TEMPORARY       =	396;
+	public static final int	PARKING_LOT_UNDERUTILIZED   =	441;
+	public static final int	PARKING_LOT_VALET           =	516;
+	public static final int	PHARMACY 					=	450;
+	public static final int	RESTAURANT_BARS 			=	361;
+
+}
