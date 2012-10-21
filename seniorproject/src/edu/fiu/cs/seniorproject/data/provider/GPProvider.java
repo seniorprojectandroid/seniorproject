909066b00c3a89f7bcd42d31f5b69f113e1f24e2
roberto <risaa002@fiu.edu>
2012-10-21 17:52:32 -0400

    Added functionality to use all the categories in the settings.

--- seniorproject/src/edu/fiu/cs/seniorproject/data/provider/GPProvider.java
+++ seniorproject/src/edu/fiu/cs/seniorproject/data/provider/GPProvider.java
@@ -3,6 +3,7 @@ package edu.fiu.cs.seniorproject.data.provider;
 import java.io.IOException;
 //import java.lang.reflect.Array;
 import java.net.MalformedURLException;
+import java.util.Hashtable;
 import java.util.LinkedList;
 //import java.util.Iterator;
 import java.util.List;
@@ -30,10 +31,139 @@ public class GPProvider extends DataProvider {
 
 	private String nextPageToken = null;
 	
+	Hashtable<String,String> categories = null;
 	public GPProvider() {
 		this.gpClient = new GPClient(AppConfig.GOOGLE_PLACE_APP_ID);
+		this.categories =  new Hashtable<String,String>();
 	}
 
+	private String getCategory(String category)
+	{
+		if(!this.getCategories().containsKey(category))
+			Logger.Error("this category: " + category + " does not exist.");
+		
+		
+		return this.getCategories().get(category);
+	}
+	
+	private Hashtable<String,String> getCategories()
+	{
+		this.categories.put(PlaceCategoryFilter.RESTAURANT_BARS.toString(), "restaurant");
+		this.categories.put(PlaceCategoryFilter.HOTEL.toString(), "lodging");
+		this.categories.put(PlaceCategoryFilter.BAKERY.toString(), "bakery");
+		this.categories.put(PlaceCategoryFilter.DENTISTS.toString(), "dentist");
+		this.categories.put(PlaceCategoryFilter.FOOD_SALES.toString(), "food");
+		this.categories.put(PlaceCategoryFilter.GALLERY_ART.toString(), "art_gallery");
+		this.categories.put(PlaceCategoryFilter.LIQUOR_SALES.toString(), "liquor_store");
+		this.categories.put(PlaceCategoryFilter.LOCKSMITH.toString(), "locksmith");
+		this.categories.put(PlaceCategoryFilter.PARKING_GARAGE.toString(), "parking");
+		this.categories.put(PlaceCategoryFilter.PARKING_LOT.toString(), "parking");
+		this.categories.put(PlaceCategoryFilter.PARKING_LOT_PROVISIONAL.toString(), "parking");
+		this.categories.put(PlaceCategoryFilter.PARKING_LOT_SELF_PARKING.toString(), "parking");
+		this.categories.put(PlaceCategoryFilter.PARKING_LOT_TEMPORARY.toString(), "parking");
+		this.categories.put(PlaceCategoryFilter.PARKING_LOT_UNDERUTILIZED.toString(), "parking");
+		this.categories.put(PlaceCategoryFilter.PARKING_LOT_VALET.toString(), "parking");
+		this.categories.put(PlaceCategoryFilter.PHARMACY.toString(), "pharmacy");
+		
+		return this.categories;
+	}
+	
+	//accounting
+	//airport
+	//amusement_park
+	//aquarium
+	//art_gallery
+	//atm
+	//bakery
+	//bank
+	//bar
+	//beauty_salon
+	//bicycle_store
+	//book_store
+	//bowling_alley
+	//bus_station
+	//cafe
+	//campground
+	//car_dealer
+	//car_rental
+	//car_repair
+	//car_wash
+	//casino
+	//cemetery
+	//church
+	//city_hall
+	//clothing_store
+	//convenience_store
+	//courthouse
+	//dentist
+	//department_store
+	//doctor
+	//electrician
+	//electronics_store
+	//embassy
+	//establishment
+	//finance
+	//fire_station
+	//florist
+	//food
+	//funeral_home
+	//furniture_store
+	//gas_station
+	//general_contractor
+	//grocery_or_supermarket
+	//gym
+	//hair_care
+	//hardware_store
+	//health
+	//hindu_temple
+	//home_goods_store
+	//hospital
+	//insurance_agency
+	//jewelry_store
+	//laundry
+	//lawyer
+	//library
+	//liquor_store
+	//local_government_office
+	//locksmith
+	//lodging
+	//meal_delivery
+	//meal_takeaway
+	//mosque
+	//movie_rental
+	//movie_theater
+	//moving_company
+	//museum
+	//night_club
+	//painter
+	//park
+	//parking
+	//pet_store
+	//pharmacy
+	//physiotherapist
+	//place_of_worship
+	//plumber
+	//police
+	//post_office
+	//real_estate_agency
+	//restaurant
+	//roofing_contractor
+	//rv_park
+	//school
+	//shoe_store
+	//shopping_mall
+	//spa
+	//stadium
+	//storage
+	//store
+	//subway_station
+	//synagogue
+	//taxi_stand
+	//train_station
+	//travel_agency
+	//university
+	//veterinary_care
+	//zoo
 	@Override
 	public Event getEventDetails(String eventId, String reference) {
 		JSONObject data = null;
@@ -327,10 +457,10 @@ public class GPProvider extends DataProvider {
 		Place place = null;
 		Location loc = null;
 
-		String categ = category.toString().toLowerCase();
+		String categ = this.getCategory(category.toString());
 		
 		String radiusInMeters = String.valueOf( Double.valueOf(radius) * 1609.34 );
-		result = getPlaces(location,"restaurant" , radiusInMeters, query);
+		result = getPlaces(location, categ, radiusInMeters, query);
 
 		if (result != null && result.length() > 0) 
 		{
