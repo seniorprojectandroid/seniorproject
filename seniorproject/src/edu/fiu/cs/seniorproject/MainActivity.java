dd42b17d2d58d196a0e2c87ed23ab4c73e6b114e
Julio <jreye087@fiu.edu>
2012-10-27 16:15:45 -0400

    Update main UI.

--- seniorproject/src/edu/fiu/cs/seniorproject/MainActivity.java
+++ seniorproject/src/edu/fiu/cs/seniorproject/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
