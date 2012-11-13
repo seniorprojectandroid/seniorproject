8b0f802fb3659abd3356d8183025d4b83e4b78d6
joeldys ortiz <jorti063@fiu.edu>
2012-11-13 15:59:42 -0500

    Some Changes done to Personalization and UI (Horizontal scrollView)

--- seniorproject/src/edu/fiu/cs/seniorproject/MainActivity.java
+++ seniorproject/src/edu/fiu/cs/seniorproject/MainActivity.java
@@ -1,13 +1,22 @@
 package edu.fiu.cs.seniorproject;
 
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.database.SQLException;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import edu.fiu.cs.seniorproject.data.MbGuideDB;
 
 public class MainActivity extends Activity {
@@ -17,6 +26,62 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); 
+        
+        
+        // scroll view
+     
+        
+        
+        TextView tv = new TextView(this);
+        tv.setText("hello");
+        
+        
+        TextView tv2 = new TextView(this);
+        tv2.setText("hello2");
+        TextView tv3 = new TextView(this);
+        tv3.setText("hello3");
+        TextView tv4 = new TextView(this);
+        tv4.setText("hello4");
+        TextView tv5 = new TextView(this);
+        tv5.setText("hello5");
+        
+        
+        
+        HorizontalScrollView hv = (HorizontalScrollView)findViewById(R.id.horizontalScrollView1);
+        ArrayList<TextView> l = new ArrayList<TextView>();
+        
+        
+        LinearLayout ly = (LinearLayout)findViewById(R.id.child_linear_layout);
+        
+        LayoutInflater inflater = (LayoutInflater)this.getLayoutInflater();
+        ImageView image = (ImageView)findViewById(R.id.image);
+        this.getResources().getDrawable(R.drawable.facebook_dark_96);
+        // image.setImageBitmap( this.getResources().getDrawable(R.drawable.facebook_dark_96));
+        
+        Bitmap bm = (Bitmap)BitmapFactory.decodeResource(this.getResources(), R.drawable.facebook_dark_96); 
+      //  image.setImageBitmap(bm);
+       //  image.setImageDrawable(this.getResources().getDrawable(R.drawable.facebook_dark_96));
+         
+         
+       // image.setImageResource(R.drawable.facebook_dark_96);
+        
+        for(int i=0; i<20; i++)
+        {      		
+        	View v = (View)inflater.inflate(R.layout.image_box_linear, null);
+        	ImageView iv =(ImageView) v.findViewById(R.id.image);
+        	TextView mtv = (TextView) v.findViewById(R.id.text);
+            if(iv!=null)
+            {
+            	iv.setImageBitmap(bm);
+            	mtv.setText("Hello "+i);
+            }    
+            
+       // View v2 = (View)inflater.inflate(R.layout.image_box_linear, null);
+        	//ly.addView(iv);
+            ly.addView(v);
+        }
+
+
         db = new MbGuideDB(this);
         
         try
@@ -98,9 +163,13 @@ public class MainActivity extends Activity {
     }
     public void onShowMyEventsClick (View view) {
     	//Intent intent = new Intent(this,MyEventsActivity.class); 
-    	Intent intent = new Intent(this,PersonalizationActivity.class); 
-    	this.startActivity(intent);
-    
-    	
+    	Intent intent = new Intent(this,MyEventsActivity.class); 
+    	this.startActivity(intent);	
     }
+    public void onItemClicked (View view) {
+    	//Intent intent = new Intent(this,MyEventsActivity.class); 
+    	Intent intent = new Intent(this,PersonalizationActivity.class); 
+    	this.startActivity(intent);	
+    }
+    
 }
